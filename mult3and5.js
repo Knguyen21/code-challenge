8c9197f1ac176ff9a2d5a4c605574ca562d9f18b
khanhnhat <nguyen.k21@gmail.com>
2016-05-06 14:22:02 -0400

    wrote function for multiples of 3 and 5

--- mult3and5.js
+++ mult3and5.js
@@ -1,3 +1,12 @@
+// Description:
+//
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+
+
+
 'use strict' ;
  function solution(num){
    let total = 0;
@@ -18,6 +27,7 @@ solution(10);
 // function solution(number){
 //   var sum = 0;
 //
+
 //   for(var i = 1;i< number; i++){
 //     if(i % 3 == 0 || i % 5 == 0){
 //       sum += i
