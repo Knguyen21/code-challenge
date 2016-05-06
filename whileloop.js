252529bb795350b640b4c6aa0957788ea64d0315
khanhnhat <nguyen.k21@gmail.com>
2016-05-06 14:29:39 -0400

    Wrote while loop to add a padding

--- whileloop.js
+++ whileloop.js
@@ -1,3 +1,12 @@
+// Task
+//
+// Coding in function padIt, function accept 2 parameters:
+//
+// 1.str, it's a string representing the string to pad, we need pad some "*" at leftside or rightside of str
+//
+// 2.n, it's a number, how many times to pad the string.
+
+
 'use strict';
 
 function padIt(str,n){
