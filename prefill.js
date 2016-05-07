0db153e396a73ef8b9436265a170418dc402990c
khanhnhat <nguyen.k21@gmail.com>
2016-05-07 19:14:47 -0400

    Create function to prefill array with 2 paramenters

--- prefill.js
+++ prefill.js
@@ -1,18 +1,34 @@
-
+// Description:
+//
+// Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+//
+// You have to validate input:
+//
+// v can be anything (primitive or otherwise)
+// if v is ommited, fill the array with undefined
+// if n is 0, return an empty array
+// if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+// When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+//
+// Code Examples
+//
+//     prefill(3,1) --> [1,1,1]
+//
+//     prefill(2,"abc") --> ['abc','abc']
+//
+//     prefill("1", 1) --> [1]
+//
+//     prefill(3, prefill(2,'2d'))
+//       --> [['2d','2d'],['2d','2d'],['2d','2d']]
+//
+//     prefill("xyz", 1)
+//       --> throws TypeError with message "xyz is invalid"
 
 function prefill(n, v) {
-console.log(isNaN(parseInt(n)), v);
-if(isNaN(parseInt(n))){
-  try {
+if(isNaN(parseInt(n)) || parseInt(n) < 0 || Number(n) % 1 !== 0){
      throw new TypeError(n + " is invalid");
-   } catch (e) {
-      console.log(e.message);
-      console.log(e.name);
-      return e.message;
-   }
  }
-  var repeat = Array.apply(n, new Array(n)).map(function(){
-    return v;});
-  console.log(repeat);
+ if(parseInt(n) === 0){return [];}
+  var repeat = Array.apply(n, new Array(n)).map(function(){return v;});
   return repeat;
 }
