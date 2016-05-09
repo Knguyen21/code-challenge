b13301f62755d8f99159cc7be2db860f49a5abc9
khanhnhat <nguyen.k21@gmail.com>
2016-05-08 20:47:01 -0400

    Write function to convert a positive number to negative number

---
+++ kyu8/opposite-number.js
@@ -0,0 +1,19 @@
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+//
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+function opposite(number) {
+  return number - (number + number);
+
+}
+
+
+//best solution
+// function opposite(number) {
+//   return(-number);
+// }
