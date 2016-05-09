013bc9b0eefca09bd78bccd4740be62206888b22
khanhnhat <nguyen.k21@gmail.com>
2016-05-08 23:24:06 -0400

    Write function to add number and convert to binary

---
+++ kyu7/addbinary.js
@@ -0,0 +1,16 @@
+// Instructions
+//
+// Write a function that takes a single string (word) as argument. The function must return
+// an ordered list containing the indexes of all capital letters in the string.
+//
+// Example
+//
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+function addBinary(a,b){
+  return (a +b).toString(2)
+}
+
+// best solutionfunction addBinary(a,b){
+//   return (a+b).toString(2)
+// }
