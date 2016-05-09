a1a491fa3b65d2cbae92e231f30fc75534564c6f
khanhnhat <nguyen.k21@gmail.com>
2016-05-08 20:56:05 -0400

    Create function that find numbers which are divisible by a given number

---
+++ kyu8/divisble.js
@@ -0,0 +1,13 @@
+// Write a function which takes two arguments and returns all number, which are divisible by given divisor. First argument is array of numbers and the second is divisor.
+//
+// Example
+//
+// divisibleBy([1,2,3,4,5,6], 2) == [2,4,6]
+
+
+// function divisibleBy(numbers, divisor){
+//   function div(n) {
+//     return n % divisor === 0;
+//   }
+//   return numbers.filter(div);
+// }
