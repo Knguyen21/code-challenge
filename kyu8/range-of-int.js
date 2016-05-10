e468060a3af1507137215fe8d537239e49e4e927
khanhnhat <nguyen.k21@gmail.com>
2016-05-10 19:07:51 -0400

    Write function to return a range of integer

---
+++ kyu8/range-of-int.js
@@ -0,0 +1,23 @@
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates
+//  a range of integers from min to max, with the step. The first integer is the minimum value, the second
+//   is the maximum of the range and the third is the step. (min < max)
+//
+// generateRange(2, 10, 2) should return array of [2,4,6,8,10] generateRange(1, 10, 3) should return
+//  array of [1,4,7,10]
+
+
+function generateRange(min, max, step){
+var arr = [];
+  for(var min; min <= max; min+=step){
+    arr.push(min);
+  }
+  return arr;
+
+}
+
+// //best solution:
+//
+// function generateRange(min, max, step) {
+//   for (var res = []; min <= max; min += step) res.push(min)
+//   return res
+// }
