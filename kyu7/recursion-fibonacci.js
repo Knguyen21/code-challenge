95287f2a37bfb729044f024dad37bde8b1c6bf16
khanhnhat <nguyen.k21@gmail.com>
2016-06-05 13:16:00 -0400

    Write recursion solution for fibonacci numbers

---
+++ kyu7/recursion-fibonacci.js
@@ -0,0 +1,32 @@
+// Fibonacci number
+//
+// In mathematical terms, the sequence f(n) of fibonacci numbers is defined by the recurrence relation
+//
+// f(n) = f(n-1) + f(n-2)
+//
+// with seed values
+//
+// f(1) = 1 and f(2) = 1
+//
+// Your task
+//
+// You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+
+
+
+
+const fibonacci = n => {
+  if(n <= 2){
+    return 1;
+  }
+  return fibonacci(n-1) + fibonacci(n-2);
+};
+
+// //best solution
+// var cache = { 1 : 1, 2 : 1}
+//
+// const fibonacci = n => cache[n] || (cache[n] = fibonacci(n - 1) + fibonacci(n - 2))
+//
+//
+//
+// const fibonacci = n => n < 2 ? n : fibonacci(--n) + fibonacci(--n);
